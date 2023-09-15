5b6c053a13ca4ab6d2f49f7f3a5371493f117682
thutasann <66902308+thutasann@users.noreply.github.com>
2023-09-15 23:01:23 +0630

    feat: theme setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from '@/components/theme/mode-toggle'
 import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   )
 }
