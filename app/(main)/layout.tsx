36ea173ffa0f1cc609fb1ed862fa0ca4160a1732
thutasann <66902308+thutasann@users.noreply.github.com>
2023-09-16 11:54:54 +0630

    fix: ci again

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import NavigationSidebar from '@/components/ui/navigation-sidebar'
 import React from 'react'
 
 interface IMainLayout {
@@ -7,7 +8,9 @@ interface IMainLayout {
 async function MainLayout({ children }: IMainLayout) {
   return (
     <div className="h-full">
-      <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0"></div>
+      <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+        <NavigationSidebar />
+      </div>
       <main className="md:pl-[72px] h-full">{children}</main>
     </div>
   )
---
+++ components/ui/navigation-sidebar.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function NavigationSidebar() {
+  return <div>NavigationSidebar</div>
+}
+
+export default NavigationSidebar
