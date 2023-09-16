5aa63e15946f986d59b30777b05d9708f8882007
thutasann <66902308+thutasann@users.noreply.github.com>
2023-09-16 11:50:17 +0630

    fix: ci

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,7 +5,12 @@ interface IMainLayout {
 }
 
 async function MainLayout({ children }: IMainLayout) {
-  return <div>{children}</div>
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0"></div>
+      <main className="md:pl-[72px] h-full">{children}</main>
+    </div>
+  )
 }
 
 export default MainLayout
