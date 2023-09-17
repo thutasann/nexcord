cfc9a6206fddbfa3600f7415b0e5aced940fa93b
thutasann <66902308+thutasann@users.noreply.github.com>
2023-09-17 15:19:37 +0630

    feat: conversation schema and chat page init

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,46 @@
+import ChatHeader from '@/components/chat/ChatHeader'
+import { currentProfile } from '@/lib/current-profile'
+import { db } from '@/lib/db'
+import { redirectToSignIn } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-function ChannelIdPage() {
-  return <div>ChannelIdPage</div>
+interface IChannelIDPage {
+  params: {
+    serverId: string
+    channelId: string
+  }
+}
+
+async function ChannelIdPage({ params: { serverId, channelId } }: IChannelIDPage) {
+  const profile = await currentProfile()
+
+  if (!profile) {
+    return redirectToSignIn()
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: channelId,
+    },
+  })
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: serverId,
+      profileId: profile.id,
+    },
+  })
+
+  if (!channel || !member) {
+    redirect('/')
+  }
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader serverId={serverId} name={channel.name} type="channel" />
+    </div>
+  )
 }
 
 export default ChannelIdPage
